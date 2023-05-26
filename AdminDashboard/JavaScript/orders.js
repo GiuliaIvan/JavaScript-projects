c9b21b48b65d0a0366f789880c4f0c4519f086a1
Giulia Ivan <89975187+GiuliaIvan@users.noreply.github.com>
2023-05-26 15:08:12 +0200

    Add files via upload

---
+++ AdminDashboard/JavaScript/orders.js
@@ -0,0 +1,50 @@
+const Orders = [
+  {
+    productName: "Foldable Mini Drone",
+    productNumber: "8531",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+
+  {
+    productName: "Lavender Mini Drone",
+    productNumber: "8531",
+    paymentStatus: "Due",
+    shipping: "Declined",
+  },
+
+  {
+    productName: "Ruko Mini Drone",
+    productNumber: "8531",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+
+  {
+    productName: "Drone Mini Drone",
+    productNumber: "8531",
+    paymentStatus: "Due",
+    shipping: "Delivered",
+  },
+
+  {
+    productName: "GPS Mini Drone",
+    productNumber: "8531",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+
+  {
+    productName: "DJI Mini Drone",
+    productNumber: "8531",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+
+  {
+    productName: "Lozenge Mini Drone",
+    productNumber: "8531",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+];
